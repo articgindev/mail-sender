fa3ca9e16129799429a5987782386bb5205b7b16
gaetanof <franciscogaetano1@gmail.com>
2024-09-28 14:07:56 -0300

    test

--- writeGS.js
+++ writeGS.js
@@ -1,28 +1,38 @@
 import { google } from 'googleapis';
 
-// Parse the credentials from the environment variable
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
-
 const auth = new google.auth.GoogleAuth({
-  credentials,
+  credentials: {
+    type: 'service_account',
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Asegúrate de reemplazar los saltos de línea correctamente
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-export const writeToSheet = async (data) => {
-  try {
-    const sheets = google.sheets({ version: 'v4', auth });
-    const spreadsheetId = process.env.VITE_SPREADSHEET_ID;
+const sheets = google.sheets({ version: 'v4', auth });
 
-    await sheets.spreadsheets.values.append({
-      spreadsheetId,
-      range: 'Sheet1!A1',
-      valueInputOption: 'USER_ENTERED',
-      resource: {
-        values: data,
-      },
-    });
-    console.log('Data written to Google Sheets successfully');
+export const writeToSheet = async (data) => {
+  const request = {
+    spreadsheetId: process.env.VITE_SPREADSHEET_ID,
+    range: 'Sheet1!A1', // Ajusta esto a tu necesidad
+    valueInputOption: 'RAW',
+    resource: {
+      values: data,
+    },
+  };
+
+  try {
+    await sheets.spreadsheets.values.append(request);
+    console.log('Datos escritos en Google Sheets.');
   } catch (error) {
-    console.error('Error writing to Google Sheets:', error);
+    console.error('Error escribiendo en Google Sheets:', error);
+    throw error;
   }
 };
