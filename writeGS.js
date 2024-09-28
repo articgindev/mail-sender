6f4b222757aeb7f7f794083b4eeaf36bdbf9feb1
gaetanof <franciscogaetano1@gmail.com>
2024-09-28 11:34:51 -0300

    test

--- writeGS.js
+++ writeGS.js
@@ -1,36 +1,27 @@
 import { google } from 'googleapis';
-import dotenv from 'dotenv';
-
-// Cargar variables de entorno
-dotenv.config();
-
-
-// Importa el archivo JSON con el assertion de tipo JSON
-import credentials from './google.json' assert { type: 'json' };
 
 const auth = new google.auth.GoogleAuth({
-  credentials,  // Ya no necesitas el keyFile, puedes pasar el objeto de credenciales directamente
+  credentials: {
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-// Función asíncrona para apendear datos en la hoja de Google Sheets
-export async function writeToSheet(values) {
-  const sheets = google.sheets({ version: 'v4', auth });
-  const spreadsheetId = process.env.VITE_SPREADSHEET_ID;
-  const range = 'Payments!A2';
-  const valueInputOption = 'USER_ENTERED';
+const sheets = google.sheets({ version: 'v4', auth });
 
-  const resource = { values };
-
+export const writeToSheet = async (data) => {
   try {
     const response = await sheets.spreadsheets.values.append({
-      spreadsheetId,
-      range,
-      valueInputOption,
-      resource,
+      spreadsheetId: process.env.VITE_SPREADSHEET_ID,
+      range: 'Payments!A1',
+      valueInputOption: 'USER_ENTERED',
+      resource: { values: data },
     });
-    console.log('Datos añadidos a Google Sheets:', response.data);
+    console.log('Datos añadidos a Google Sheets:', response.data.updates);
+    return response.data.updates;
   } catch (error) {
-    console.error('Error al escribir en Google Sheets:', error);
+    console.error('Error escribiendo en Google Sheets:', error);
+    throw error;
   }
-}
+};
