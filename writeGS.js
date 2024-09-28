b20f965ed45784a406e05f66eec88e5f3d7c5bba
gaetanof <franciscogaetano1@gmail.com>
2024-09-28 18:21:15 -0300

    test

--- writeGS.js
+++ writeGS.js
@@ -1,18 +1,11 @@
 import { google } from 'googleapis';
 
+
+// Importa el archivo JSON con el assertion de tipo JSON
+import credentials from './google.json' assert { type: 'json' };
+
 const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: 'service_account',
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Asegúrate de reemplazar los saltos de línea correctamente
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-  },
+  credentials,  // Ya no necesitas el keyFile, puedes pasar el objeto de credenciales directamente
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
